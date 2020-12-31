c5bcf92f12e55248c6735314945e4157d74ca63c
Артем Ульянов <mc2017artemklochkcov435@yandex.ru>
2020-12-31 13:48:18 +0300

    ladno

--- messages_handler.py
+++ messages_handler.py
@@ -73,7 +73,7 @@ def get_answer(body, from_id, payload=None):
                     except:
                         pass
                     if distance == 0:
-                        message, attachment, keyboard = await c.process(arg['notsystem_vars'])
+                        message, attachment, keyboard = c.process(arg['notsystem_vars'])
                         arg['notsystem_vars'].clear()
                         return message, attachment, keyboard
                 else:
