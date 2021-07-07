a54c6aa7dfb1b0440d8f4aad77a9f6dd35dc4c9c
Артем Ульянов <>
2021-07-08 00:03:50 +0300

    sex

--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+from logging import error
 from config import app, db, group_config, session, session_papochka
 from models import Person as Passport
 from flask import request
@@ -52,4 +53,5 @@ def botResp():
 
 @app.errorhandler(500)
 def handler(e):
+    error(e)
     return 'ok'
